5225ec7222bc81239e721e9d99d31cbe49a2c3ab
Roman_Iovlev <roman_iovlev@epam.com>
2019-11-04 21:52:14 +0300

    v 1.0 integration

--- src/test/java/org/mytests/tests/TestsInit.java
+++ src/test/java/org/mytests/tests/TestsInit.java
@@ -1,6 +1,5 @@
 package org.mytests.tests;
 
-import com.applitools.eyes.selenium.Eyes;
 import org.mytests.uiobjects.example.site.SiteJdi;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -8,45 +7,26 @@ import org.testng.annotations.BeforeSuite;
 
 import java.lang.reflect.Method;
 
-import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
-import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
+import static com.epam.jdi.eyes.JDIEyes.*;
 import static com.epam.jdi.light.elements.init.PageFactory.initSite;
 import static com.epam.jdi.light.settings.WebSettings.logger;
-import static java.lang.String.format;
 import static org.mytests.uiobjects.example.site.SiteJdi.homePage;
 
 public class TestsInit {
-    public static Eyes eyes = defaultEyes();
 
     @BeforeSuite(alwaysRun = true)
     public static void setUp() {
         initSite(SiteJdi.class);
+        initVisualTest();
         homePage.open();
         logger.info("Run Tests");
     }
     @BeforeMethod
     public void before(Method method) {
-        String testName = format("%s.%s", method.getDeclaringClass().getSimpleName(), method.getName());
-        eyes.open(getDriver(), "JDI integration with Applitools", testName);
+        newVisualTest(method);
     }
     @AfterSuite(alwaysRun = true)
     public static void teardown() {
-        try {
-            if (eyes.getIsOpen())
-                eyes.close();
-        } finally {
-            if (eyes.getIsOpen())
-                eyes.abortIfNotClosed();
-        }
-        killAllSeleniumDrivers();
-    }
-
-    private static Eyes defaultEyes() {
-        Eyes eyes = new Eyes();
-        String apiKey = System.getenv("APPLITOOLS_API_KEY");
-        if (apiKey == null)
-            throw new RuntimeException("Please specify APPLITOOLS_API_KEY in Environment variables");
-        eyes.setApiKey(apiKey);
-        return eyes;
+        closeAllEyes();
     }
 }
--- src/test/java/org/mytests/tests/example/FormTests.java
+++ src/test/java/org/mytests/tests/example/FormTests.java
@@ -1,14 +1,10 @@
 package org.mytests.tests.example;
 
-import com.applitools.eyes.Region;
 import org.mytests.tests.TestsInit;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Rectangle;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
 import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.hamcrest.Matchers.containsString;
 import static org.mytests.uiobjects.example.entities.Defaults.DEFAULT_USER;
 import static org.mytests.uiobjects.example.entities.Defaults.SPIDER_MAN;
 import static org.mytests.uiobjects.example.site.SiteJdi.*;
@@ -17,27 +13,16 @@ import static org.mytests.uiobjects.example.site.pages.MarvelousPage.userTable;
 public class FormTests extends TestsInit {
     @Test
     public void verifyUsersTest() {
-        eyes.checkWindow(homePage.getName());
         userIcon.click();
         loginForm.loginAs(DEFAULT_USER);
-        //eyes.checkElement(userName.core().get());
-        WebElement userName = getDriver().findElement(By.id("user-name"));
-        eyes.checkElement(userName, "Validation Element");
-        eyes.checkRegion(userName, "Validation Region from Element");
-        //left, top, width, height
-        Rectangle region = userName.getRect();
-        eyes.checkRegion(new Region(region.x, region.y, region.width, region.height),
-        -1, "Validation Region");
-        eyes.checkRegion(By.id("user-name"), "Validation Locator");
-
         marvelousPage.open();
-        eyes.checkWindow(marvelousPage.getName());
-
         userTable.assertThat()
             .all().rows(d -> d.user.length() > 4)
             .no().rows(d -> isBlank(d.user))
             .atLeast(3).rows(d -> d.type.contains("User"))
-            .exact(2).rows(d -> d.user.contains("R"))
             .exact(1).rows(SPIDER_MAN);
+        userTable.line(2).description.image.is()
+            .src(containsString("spider-man"));
+        userTable.line(3).description.is().displayed();
     }
 }
